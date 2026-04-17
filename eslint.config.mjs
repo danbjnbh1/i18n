add5ff94c7aac854d683d583b9d55ffb28e06fbf
Dan Nachmany <dan.nachmany@guesty.com>
2026-04-18 00:30:07 +0300

    add es lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
+import prettier from "eslint-plugin-prettier/recommended";
 
 export default tseslint.config(
   {
@@ -8,6 +9,7 @@ export default tseslint.config(
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
+  prettier,
   {
     languageOptions: {
       ecmaVersion: 2022,
